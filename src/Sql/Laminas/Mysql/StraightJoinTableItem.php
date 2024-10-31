3589854374615fcf4be7563ae64b875f9c44c357
Jasper van Gestel <jvangestel@gmail.com>
2024-10-31 20:54:45 +0100

    Add a mysql specific straight join translator to translate inner joins to straight_joins in specific optimalization scenario's

---
+++ src/Sql/Laminas/Mysql/StraightJoinTableItem.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zalt\Model\Sql\Laminas;
+
+use Zalt\Model\Sql\JoinTableItem;
+
+class StraightJoinTableItem extends JoinTableItem
+{
+    public function __construct(
+        protected readonly string $joinTable,
+        protected array $joinFields,
+        protected readonly bool $joinInner = true,
+        protected readonly ?string $tableAlias = null,
+        protected readonly bool $straight = false,
+    )
+    {
+        parent::__construct($this->joinTable, $this->joinFields, $this->joinInner, $this->tableAlias);
+    }
+}
